3ae75c42a69e3551ebf7970177e240b9830902f3
вNatalya Abrosimova <oxidena86@gmail.com>
2022-11-08 16:27:45 +0700

    add test_2

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('second_page/', views.second_page, name='second_page'),
+    path('', include('infra_app.urls', namespace='infra_app')),
+    path('admin/', admin.site.urls),
 ]
